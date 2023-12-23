a32747904cdae51b93c87f5891dd972ae50c11e4
famousyub <famousmeyub@gmail.com>
2023-12-23 14:58:32 +0100

    'fylmi

---
+++ navs.php
@@ -0,0 +1,222 @@
+<?php
+?>
+<style>
+.first {
+	background-color:#233a50;
+	color:#233a50;
+}
+	</style>
+
+<!-- BEGIN | Header -->
+<header class="ht-header">
+	<div class="container">
+		<nav class="navbar navbar-default navbar-custom">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header logo">
+				    <div class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					    <span class="sr-only">#
+							<img   src="./images/logop.png" height="58" width="115" />
+						</span>
+					    <div id="nav-icon1">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+				    </div>
+				    <a href="index-2.html"><img class="logo" src="images/logop.png" alt="" width="180px" height="60px"></a>
+			    </div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse flex-parent" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav flex-child-menu menu-left">
+						<li class="hidden">
+							<a href="#page-top">
+							<img   src="./images/logop.png" height="58" width="115" />
+							</a>
+						</li>
+
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown">
+							films  <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="index-2.html">tous</a></li>
+								<li><a href="homev2.html">filter</a></li>
+								<li><a href="homev3.html">nouveaute</a></li>
+								<li><a href="index-2.html">collection</a></li>
+								<li><a href="homev2.html"> film hd</a></li>
+								<li><a href="homev3.html">Films VOSTFR </a></li>
+                                <li><a href="homev3.html">Films Arabe</a></li>
+								<li><a href="index-2.html">Films 3D </a></li>
+								<li><a href="homev2.html"> Films  Ulta HD (k)</a></li>
+								<li><a href="homev3.html">Dernier ajout </a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown">
+							Series <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+                            <li><a href="index-2.html">tous</a></li>
+								<li><a href="homev2.html">filter</a></li>
+								<li><a href="homev3.html">Nouveautés</a></li>
+								<li><a href="index-2.html">collection</a></li>
+                                <li><a href="homev3.html">Series VOSTFR </a></li>
+                                <li><a href="homev3.html">Series Arabe</a></li>
+								<li><a href="index-2.html">Series 3D </a></li>
+								<li><a href="homev2.html"> Series  Ulta HD (4k)</a></li>
+								<li><a href="index-2.html">Series Terminés</a></li>
+								<li><a href="homev2.html">Dernier ajout</a></li>
+								<li><a href="homev3.html"> </a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							Animes<i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" >anime <i class="ion-ios-arrow-forward"></i></a>
+									<ul class="dropdown-menu level2">
+                                    <li><a href="index-2.html">tous</a></li>
+								
+                                <li><a href="homev3.html">filter </a></li>
+										<li><a href="moviegrid.html">Nouveautés</a></li>
+										<li><a href="moviegridfw.html">film anime g</a></li>
+										<li><a href="moviegrid.html">series  Anime</a></li>
+										<li><a href="moviegridfw.html">dernir ajout  </a></li>
+									</ul>
+								</li>			
+								<li><a href="movielist.html">Movie list</a></li>
+								<li><a href="moviesingle.html">Movie single</a></li>
+								<li class="it-last"><a href="seriessingle.html">Series tv</a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							games <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="celebritygrid01.html">pc games</a></li>
+								<li><a href="celebritygrid02.html">xbox game</a></li>
+								<li><a href="celebritylist.html">games list</a></li>
+								<li class="it-last"><a href="celebritysingle.html">all</a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							Documentaires <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="celebritygrid01.html">tous </a></li>
+								<li><a href="celebritygrid02.html">filter</a></li>
+								<li><a href="celebritylist.html"></a></li>
+								<li class="it-last"><a href="celebritysingle.html"> </a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							Spectacles  <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="bloglist.html">tous </a></li>
+								<li><a href="bloggrid.html">filtre </a></li>
+                                <li><a href="bloglist.html">Nouveautés</a></li>
+								<li><a href="bloggrid.html">Theatre </a></li>
+                                <li><a href="bloglist.html">concert</a></li>
+								<li><a href="bloggrid.html">Divertissements</a></li>
+                                <li><a href="bloglist.html">Humour</a></li>
+								<li><a href="bloggrid.html">Télé reélité</a></li>
+                                <li><a href="bloglist.html">tv show </a></li>
+								<li><a href="bloggrid.html"> sport </a></li>
+								<li class="it-last"><a href="blogdetail.html">derniers ajouts</a></li>
+							</ul>
+						</li>
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							jeux <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="userfavoritegrid.html">tous</a></li>
+								<li><a href="userfavoritelist.html">Nouveautés  </a></li>
+								<li><a href="userprofile.html">Pc</a></li>
+								<li class="it-last"><a href="userrate.html">PS4</a></li>
+                                <li><a href="userfavoritegrid.html">PS3</a></li>
+								<li><a href="userfavoritelist.html">Ninendo switch</a></li>
+								<li><a href="userprofile.html">Mac</a></li>
+								<li class="it-last"><a href="userrate.html">IOS</a></li>
+                                <li><a href="userprofile.html">Wii</a></li>
+								<li class="it-last"><a href="userrate.html">Android </a></li>
+                                <li class="it-last"><a href="userrate.html">Derniers ajouts  </a></li>
+							</ul>
+						</li>
+                        <li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							Musiques <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="userfavoritegrid.html">tous</a></li>
+								<li><a href="userfavoritelist.html">filter  </a></li>
+								<li><a href="userprofile.html"> Nouveautés</a></li>
+								<li class="it-last"><a href="userrate.html">Albums </a></li>
+                                <li><a href="userfavoritegrid.html">B-O-O-S-T</a></li>
+								<li><a href="userfavoritelist.html">Compilations</a></li>
+								<li><a href="userprofile.html">Mac</a></li>
+								<li class="it-last"><a href="userrate.html">Concert</a></li>
+                                <li><a href="userprofile.html">Discographies</a></li>
+							
+                                <li class="it-last"><a href="userrate.html">Derniers ajouts  </a></li>
+							</ul>
+						</li>
+                        <li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							Logiciels <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="userfavoritegrid.html">tous</a></li>
+								<li><a href="userfavoritelist.html">Filter   </a></li>
+								<li><a href="userprofile.html">Nouveautés</a></li>
+								<li class="it-last"><a href="userrate.html">Microsoft</a></li>
+                                <li><a href="userfavoritegrid.html">Mac</a></li>
+								<li><a href="userfavoritelist.html">Android</a></li>
+								<li><a href="userprofile.html">IOS</a></li>
+							
+                                <li class="it-last"><a href="userrate.html">Derniers ajouts  </a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav flex-child-menu menu-right">
+						<li class="dropdown first">
+							<a class="btn btn-default dropdown-toggle lv1" data-toggle="dropdown" data-hover="dropdown">
+							eBooks <i class="fa fa-angle-down" aria-hidden="true"></i>
+							</a>
+							<ul class="dropdown-menu level1">
+								<li><a href="landing.html">tous</a></li>
+								<li><a href="404.html">filter </a></li>
+								<li class="it-last"><a href="comingsoon.html">Nouveautés</a></li>
+								<li><a href="landing.html">BO</a></li>
+								<li><a href="404.html">journaux  </a></li>
+								<li class="it-last"><a href="comingsoon.html">books & audio </a></li>
+                                <li><a href="landing.html">Magazines</a></li>
+								<li><a href="404.html">Mangas  </a></li>
+								<li class="it-last"><a href="comingsoon.html">Derniers ajouts  </a></li>
+							</ul>
+						</li>                
+						<li><a href="#">Help#contact</a></li>
+						<li class="loginLink"><a href="#">LOG In</a></li>
+						<li class="btn signupLink"><a href="#">sign up</a></li>
+					</ul>
+				</div>
+			<!-- /.navbar-collapse -->
+	    </nav>
+	    
+	    <!-- top search form -->
+	    <div class="top-search">
+	    	<select>
+				<option value="united">TV show</option>
+				<option value="saab">Others</option>
+			</select>
+			<input type="text" placeholder="Search for a movie, TV Show or celebrity that you are looking for">
+	    </div>
+	</div>
+</header>
---
+++ script.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+include("header.php");
+
+
+include("preloading.php");
+
+
+include("header.php");
+
+include("navs.php");
+include ("section.php");
+
+include("footer.php");
+
